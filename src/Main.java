0f6ae933e3f423a7cda5f814d1d6d15521ab2df5
Step X <stepan170297@gmail.com>
2023-11-29 20:50:43 +0400

    thierd commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         int weight = 111;
-        double  height = 2.03;
-        double index = service.calculate(weight,height);
+        double height = 2.03;
+        double index = service.calculate(weight, height);
         System.out.println(index);
     }
 }

9adcbdd3d46ee0ba77fa12e2cda52e3084cc8b9b
Step X <stepan170297@gmail.com>
2023-11-29 20:47:08 +0400

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,3 +1,11 @@
 public class BmiService {
+    public double calculate(int weight, double height) {
+        weight = 111;
+        height = 2.03;
+        double index = weight / (height * height);
+        return (int) index;
+
+    }
+
 
 }
